606a8edd2d8850b7b83095cec1e499ab4f550afe
Tati2327 <tatumora1328@gmail.com>
2019-03-17 11:48:49 -0600

    Subiendo archivo del servidor y cliente...

---
+++ Scrabble_Proyecto1/Server.h
@@ -0,0 +1,41 @@
+//
+// Created by tati2327 on 17/03/19.
+//
+
+#ifndef SCRABBLE_PROYECTO1_SERVER_H
+#define SCRABBLE_PROYECTO1_SERVER_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+/*!
+ *Un servidor simple en el dominio de internet usando TCP
+ * El número de puerto se pasa como un argumento
+ * Esta versión se ejecuta para siempre, forking off separado
+ * proceso para cada conexión
+ *
+ */
+class Server {
+
+    public:
+
+        int sockfd, newsockfd, portno, pid;
+        socklen_t clilen;
+        struct sockaddr_in serv_addr, cli_addr;
+
+        void error(const char *msg);
+        int newServer(int argc, char *argv[]);
+
+        /******** DOSTUFF() *********************
+         There is a separate instance of this function
+         for each connection.  It handles all communication
+         once a connnection has been established.
+         *****************************************/
+        void dostuff (int sock);
+};
+#endif //SCRABBLE_PROYECTO1_SERVER_H
---
+++ Scrabble_Proyecto1/cmake-build-debug/CMakeFiles/Scrabble_Proyecto1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Scrabble_Proyecto1.dir/main.cpp.o"
+  "CMakeFiles/Scrabble_Proyecto1.dir/Server.cpp.o"
+  "CMakeFiles/Scrabble_Proyecto1.dir/Client.cpp.o"
+  "Scrabble_Proyecto1.pdb"
+  "Scrabble_Proyecto1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Scrabble_Proyecto1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
