b50e520b2d42e18bea10700546817eef8ba96d5b
Tati2327 <tatumora1328@gmail.com>
2019-03-23 23:38:00 -0600

    Se trabajo en la carpeta PruebaThread y se modificó el cmake para que los hilos funcionaran

--- PruebaThread/main.cpp
+++ PruebaThread/main.cpp
@@ -1,37 +1,31 @@
-#include <iostream>
+// thread example
+#include <iostream>       // std::cout
+#include <thread>         // std::thread
 
 using namespace std;
 
+void foo()
+{
+    cout<<"HHHHHHHHH"<<endl;
+}
 
+void bar(int x)
+{
+    cout<<"JJJJJJJJJJJJJ"<<endl;
+}
 
-int main(){
-    /*int aa[4]={1,2,3,4};
-    int *ptr2=(int*)(&aa+1);
-    int java;
-    cout<<aa+1<<endl;
-    for(int i=0; i< sizeof(*aa);i++){
-        java+=aa[i] + *(ptr2-1);
-    }
-    cout<<java<<","<<*(ptr2-1)<<endl;*/
-    int i [ ][5] = {1,2,3,4,5,6,7,8,9,10};
-    for (int b=0; b < 10; b++){
-        cout<<*i[b]<<endl;
-    }
+int main()
+{
+    std::thread first (foo);     // spawn new thread that calls foo()
+    std::thread second (bar,0);  // spawn new thread that calls bar(0)
 
-    /*int t=1;
-    int *i = &t;
-    int *f = &t+1;
-    int h = 8;
+    std::cout << "main, foo and bar now execute concurrently...\n";
 
-    cout<<f<<endl;
-    *f = *i;
-    cout<<f<<endl;
-    cout<<*f<<endl;
+    // synchronize threads:
+    first.join();                // pauses until first finishes
+    second.join();               // pauses until second finishes
 
-    cout<<*i<<endl;
-    cout<<i<<endl;
-    *i-- = h;
-    cout<<*i<<endl;
-    cout<<i<<endl;*/
+    std::cout << "foo and bar completed.\n";
 
+    return 0;
 }
